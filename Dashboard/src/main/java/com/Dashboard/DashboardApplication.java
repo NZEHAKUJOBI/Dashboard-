accce7fa97b1c82c4139dc30087d0cc2d5f4f9a0
nzeh daniel <nzehakujobi@gmail.com>
2024-06-25 07:36:46 +0100

    ADDDED LIquid Base

--- Dashboard/src/main/java/com/Dashboard/DashboardApplication.java
+++ Dashboard/src/main/java/com/Dashboard/DashboardApplication.java
@@ -1,13 +1,44 @@
 package com.Dashboard;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class DashboardApplication {
+
+	@Value("${spring.datasource.url}")
+	private String databaseUrl;
+
+	@Value("${spring.datasource.username}")
+	private String databaseUsername;
+
+	@Value("${spring.datasource.password}")
+	private String databasePassword;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardApplication.class, args);
 	}
 
+	@Bean
+	public SpringLiquibase liquibase() {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:changelog/master.xml");
+		liquibase.setDataSource(dataSource());
+		return liquibase;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(databaseUsername);
+        dataSource.setPassword(databasePassword);
+        return dataSource;
+	}
 }
---
+++ Dashboard/src/main/java/com/Dashboard/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.Dashboard.Repository;
+import com.Dashboard.Model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+}
